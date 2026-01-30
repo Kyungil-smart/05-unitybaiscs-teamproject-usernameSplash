2c597281c5851c3e77bf0c8f673637fc319d22c2
J0_km <skykm0405@naver.com>
2026-01-30 18:37:26 +0900

    몬스터 이동 애니메이션 수정

--- TeamProject01/Assets/Scripts/MonsterMove.cs
+++ TeamProject01/Assets/Scripts/MonsterMove.cs
@@ -8,12 +8,14 @@ public class MonsterMove : MonoBehaviour
     [SerializeField] private NavMeshAgent nav;
     [SerializeField] private Transform player;
     [SerializeField] private CharacterHealth healthController;
+    [SerializeField] private Animator animator;
 
     private void Awake()
     {
         nav = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
         healthController = GetComponent<CharacterHealth>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
@@ -24,5 +26,7 @@ public class MonsterMove : MonoBehaviour
         {
             nav.SetDestination(player.position);
         }
+
+        animator.SetFloat("Speed", nav.velocity.magnitude);
     }
 }
